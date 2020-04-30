6cd97e0b70326c3d462b93ba4d6ada695fd2fd99
Nicholas Keller <nic.keller328@gmail.com>
2020-04-30 13:41:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.find("rating = ?", self.highest_rating).first
+    self.where("rating = ?", self.highest_rating).first
   end
 end
